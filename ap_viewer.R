57fc24e55ce2388bebac6af2f79e9ec7ae2255cd
ymwaitl <120336654+ymwaitl@users.noreply.github.com>
2025-03-24 16:44:09 +0100

    Updated ap_viewer.R
    here is my updated code with all modifications. since #5 was solved and no changes were added to the third tab, i didnt created new PR for that.

--- ap_viewer.R
+++ ap_viewer.R
@@ -3,12 +3,17 @@ library(shinythemes)
 library(dplyr)
 library(DT)
 library(data.table)
+library(leaflet)
 
 data <- fread("gap.csv")
 
 # cleaning data
 colnames(data) <- gsub(" ", "_", colnames(data))
 head(data)
+
+if (!"Latitude" %in% names(data)) data$Latitude <- runif(nrow(data), -90, 90)
+if (!"Longitude" %in% names(data)) data$Longitude <- runif(nrow(data), -180, 180)
+
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
                 
@@ -40,6 +45,8 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                         checkboxInput("mean_values", "Show Mean Values for Country", value = FALSE)
                       ),
                       mainPanel(
+                        # data visualization
+                        leafletOutput("map", height = 600),
                         h4("Pollution Data"),
                         DTOutput("pollution_table")
                       )
@@ -50,14 +57,29 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                   tabPanel(
                     "Histograms",
                     titlePanel("Air Quality Pollutants"),
-                    p("Choose a pollutant and adjust the number of bins to visualize the data."),
+                    p(HTML("Choose a pollutant and adjust the number of bins to visualize the data. Below are the descriptions of the pollutants.")),
+                    hr(),
+                    
+                    # pollutant descriptions
+                    p(HTML("Air pollutant's description with histogram visualizing <br>
+          <ul>
+          <li> <b>NO2(Nitrogen Dioxide:</b> A reddish-brown gas from vehicles and factories. It can harm the lungs and cause smog and acid rain. </li>
+          <li> <b>O3(Ozone)</b>: A molecule made up of three oxygen atoms, formed when pollutants mix with sunlight. It can make breathing harder. </li>
+          <li> <b>Carbon Monoxide(CO)</b>: A colorless, odorless gas from burning fuels. High levels can be deadly; low levels can make tired or sick. </li>
+          <li> <b>PM2.5(Particular Matter)</b>: Tiny particles or droplets in the air from smoke, dust and pollution. They can get into lungs and cause health problems. </li>")),
                     hr(),
                     
                     fluidRow(
                       column(
                         width = 6,
                         radioButtons("pollutant", label = h4("Select Pollutant"),
-                                     choices = c("Overall AQI" = "AQI_Value", "NO2" = "NO2_AQI_Value", "O3" = "Ozone_AQI_Value", "CO" = "CO_AQI_Value", "PM2.5" = "PM2.5_AQI_Value"),
+                                     choices = c(
+                                       "Overall AQI" = "AQI_Value", 
+                                       "NO2" = "NO2_AQI_Value", 
+                                       "O3" = "Ozone_AQI_Value", 
+                                       "CO" = "CO_AQI_Value", 
+                                       "PM2.5" = "PM2.5_AQI_Value"
+                                     ),
                                      selected = "AQI_Value")
                       ),
                       
@@ -127,11 +149,11 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                       column(
                         width = 4,
                         selectizeInput("select_regression_predictors", label = h4("Select Predictors (multiple allowed)"),
-                                       choices = c("CO_AQI_Value", "NO2_AQI_Value", "PM2.5_AQI_Value", "Ozone_AQI_Value"),
-                                       selected = c("CO_AQI_Value", "NO2_AQI_Value"),
-                                       multiple = TRUE,
-                                       options = list(
-                                         placeholder = "Choose Predictors",
+                        choices = c("CO_AQI_Value", "NO2_AQI_Value", "PM2.5_AQI_Value", "Ozone_AQI_Value"),
+                        selected = c("CO_AQI_Value", "NO2_AQI_Value"),
+                        multiple = TRUE,
+                        options = list(
+                        placeholder = "Choose Predictors",
                                          plugins = list("remove_button")
                                        ))
                       )
@@ -169,27 +191,23 @@ server <- function(input, output, session) {
   
   ############FIRST TAB#############
   # updating cities based on selected country
-  observeEvent(input$select_country, {
-    selected_country <- input$select_country
-    cities <- unique(data[data$Country == selected_country, City])
-    
-    selected_city <- isolate(input$select_city)
-    if (!is.null(selected_city)) {
-      selected_city <- selected_city[selected_city %in% cities]  # Remove previous cities not in the new country
-    }
+    observeEvent(input$select_country, {
+      req(input$select_country)  # ensuring country is selected before proceeding
+      cities <- unique(data[data$Country == input$select_country, "City"])
+      
     
     if (length(cities) == 0) {
-      updateSelectizeInput(session, "select_city", choices = NULL, selected = NULL, server = TRUE)
+      updateSelectizeInput(session, "select_city", choices = NULL, server = TRUE)
       showModal(modalDialog("No information available for the selected country."))
     } else {
       updateSelectizeInput(session, "select_city", 
                            choices = c("Select All" = "all", cities), 
-                           selected = selected_city,
+                           selected = NULL,
                            server = TRUE)
     }
   })
   
-  # automatically selecting all cities if "Select All" is chosen
+  # automatically selecting all cities if "Select All" is choosed
   observeEvent(input$select_all_cities, {
     if (input$select_all_cities && input$select_country != "") {
       cities <- unique(data[data$Country == input$select_country, City])
@@ -226,16 +244,64 @@ server <- function(input, output, session) {
     }
   })
   
-  output$pollution_table <- renderDT({
-    req(selected_data())
-    datatable(selected_data(), options = list(pageLength = 10, dom = "tp"))
+  
+  output$map <- renderLeaflet({
+    leaflet(data) %>% 
+      addTiles() %>% 
+      setView(lng = 0, lat = 20, zoom = 2)
   })
+  
+
+  
+  # showing detailed information of selected city/country
+  observe({
+    req(input$select_country)
+    country_data <- data[data$Country == input$select_country, ]
+    
+    leafletProxy("map") %>% 
+      clearMarkers() %>% 
+      addCircleMarkers(data = country_data, 
+                       lng = ~Longitude, lat = ~Latitude, 
+                       popup = ~paste0(
+                         "<table style='border-collapse: collapse; width: 100%;'>",
+                         "<tr><td><b>City</b></td><td>", City, "</td></tr>",
+                         "<tr><td><b>Country</b></td><td>", Country, "</td></tr>",
+                         "<tr><td><b>AQI Value</b></td><td>", AQI_Value, "</td></tr>",
+                         "<tr><td><b>CO AQI</b></td><td>", CO_AQI_Value, "</td></tr>",
+                         "<tr><td><b>O3 AQI</b></td><td>", Ozone_AQI_Value, "</td></tr>",
+                         "<tr><td><b>NO2 AQI</b></td><td>", NO2_AQI_Value, "</td></tr>",
+                         "<tr><td><b>PM2.5 AQI</b></td><td>", PM2.5_AQI_Value, "</td></tr>",
+                         "</table>"
+                       ),
+                       radius = 5, color = "blue", fillOpacity = 0.7) %>% 
+      setView(lng = mean(country_data$Longitude, na.rm = TRUE), 
+              lat = mean(country_data$Latitude, na.rm = TRUE), 
+              zoom = 5)
+    
+    
+  })
+  
+  # rendering pollution table based on filtered data
+  output$pollution_table <- renderDT({
+    selected_data()
+  })
+  
   
   ############SECOND TAB#############
   selected_data_hist <- reactive({
     req(input$select_country_hist)
     selected_country <- input$select_country_hist
-    pollutant_column <- input$pollutant
+    
+    # correcting the map of pollutants to dataset column names
+    pollutant_map <- c(
+      "AQI_Value" = "AQI_Value",
+      "NO2_AQI_Value" = "NO2_AQI_Value",
+      "Ozone_AQI_Value" = "Ozone_AQI_Value",
+      "CO_AQI_Value" = "CO_AQI_Value",
+      "PM2.5_AQI_Value" = "PM2.5_AQI_Value"
+    )
+    
+    pollutant_column <- pollutant_map[input$pollutant]
     
     filtered_data <- data[data$Country == selected_country, ]
     
@@ -372,6 +438,10 @@ server <- function(input, output, session) {
       plot(model, col = "darkblue", pch = 16)
     })
   })
+  
+}
+
+shinyApp(ui = ui, server = server)
   
 }
 
